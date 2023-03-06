14b26fe43cca819bcb033df4fe6e1afde70db377
lizaskripnikova <skripnikovaliza@gmail.com>
2023-03-06 18:44:13 +1100

    dice

--- dice.test.js/dice.js
+++ dice.test.js/dice.js
@@ -4,17 +4,16 @@ let resultElement6 = document.getElementById("result6");
 let rollButton10 = document.getElementById("rollButton10");
 let resultElement10 = document.getElementById("result10");
 
-// Add event listeners to the buttons using on[event] property
+// Generate a random number between 1 and 6 
 rollButton6.onclick = function() {
-  // Generate a random number between 1 and 6 (inclusive)
   let diceRoll = Math.floor(Math.random() * 6) + 1;
   
   // Display the result on the web page
   resultElement6.textContent = "You rolled a " + diceRoll + "!";
 };
 
+// Generate a random number between 1 and 10 
 rollButton10.onclick = function() {
-  // Generate a random number between 1 and 10 (inclusive)
   let diceRoll = Math.floor(Math.random() * 10) + 1;
   
   // Display the result on the web page
