69ecf4cae1436e8a0c10aaeeecc625b2c779af61
lizaskripnikova <skripnikovaliza@gmail.com>
2023-03-10 16:41:22 +1100

    new new dice

--- dice.test.js/dice.js
+++ dice.test.js/dice.js
@@ -1,21 +1,26 @@
 // Get references to the buttons and result elements
-let rollButton6 = document.getElementById("rollButton6");
-let resultElement6 = document.getElementById("result6");
-let rollButton10 = document.getElementById("rollButton10");
-let resultElement10 = document.getElementById("result10");
+const rollButton6 = document.getElementById("rollButton6");
+const resultElement6 = document.getElementById("result6");
+const rollButton10 = document.getElementById("rollButton10");
+const resultElement10 = document.getElementById("result10");
 
-// Generate a random number between 1 and 6 
-rollButton6.onclick = function() {
-  let diceRoll = Math.floor(Math.random() * 6) + 1;
+
+function rollDice(maxRoll) {
+  let diceRoll = Math.floor(Math.random() * maxRoll) + 1;
   
   // Display the result on the web page
-  resultElement6.textContent = "You rolled a " + diceRoll + "!";
+  if (maxRoll === 6) {
+    resultElement6.textContent = "You rolled a " + diceRoll + "!";
+  } else {
+    resultElement10.textContent = "You rolled a " + diceRoll + "!";
+  }
+}
+
+// To call the rollDice function
+rollButton6.onclick = function() {
+  rollDice(6);
 };
 
-// Generate a random number between 1 and 10 
 rollButton10.onclick = function() {
-  let diceRoll = Math.floor(Math.random() * 10) + 1;
-  
-  // Display the result on the web page
-  resultElement10.textContent = "You rolled a " + diceRoll + "!";
+  rollDice(10);
 };
