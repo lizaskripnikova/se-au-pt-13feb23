5b7747a7d182646bb207a8d8b0419b3d156ac5ae
lizaskripnikova <skripnikovaliza@gmail.com>
2023-03-10 16:46:28 +1100

    testing

---
+++ script.js
@@ -0,0 +1,13 @@
+let myArr = []
+let myArr1 = ["1","Teddy", "Luke", 20, 50, 100]
+console.log(myArr1)
+myArr.push(30)
+
+let firstArr = []
+for (let index = 0; index < 2; index 1++)
+{
+    newArr.push(myArr1[index])
+}
+
+newArr1.push(30)
+
