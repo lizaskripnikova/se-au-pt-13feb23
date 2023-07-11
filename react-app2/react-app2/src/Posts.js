da6956d207b725c032b6c226bb43f9711897656d
lizaskripnikova <skripnikovaliza@gmail.com>
2023-07-11 11:10:00 +1000

    lab7

---
+++ react-app2/react-app2/src/Posts.js
@@ -0,0 +1,13 @@
+// module 7 slide 46
+import React from 'react';
+
+const Posts = () => {
+  return (
+    <div>
+      <h1>Posts Page</h1>
+      {/* Add your posts content here */}
+    </div>
+  );
+};
+
+export default Posts;
