966f754ad6e86ab659d60c14597a8bee536d9859
lizaskripnikova <skripnikovaliza@gmail.com>
2023-08-02 11:24:57 +1000

    capstone

--- weatherapp/src/registerServiceWorker.js
+++ weatherapp/src/registerServiceWorker.js
@@ -52,15 +52,12 @@ function registerValidSW(swUrl) {
         installingWorker.onstatechange = () => {
           if (installingWorker.state === 'installed') {
             if (navigator.serviceWorker.controller) {
-              // At this point, the old content will have been purged and
-              // the fresh content will have been added to the cache.
-              // It's the perfect time to display a "New content is
-              // available; please refresh." message in your web app.
+              // The old content will have been purged and the fresh content will have been added to the cache.
+              // A message will be displayed a "New content is available; please refresh." message in your web app.
               console.log('New content is available; please refresh.');
             } else {
-              // At this point, everything has been precached.
-              // It's the perfect time to display a
-              // "Content is cached for offline use." message.
+              // Everything has been precached.
+              // A message will be displayed "Content is cached for offline use." message.
               console.log('Content is cached for offline use.');
             }
           }
@@ -81,7 +78,7 @@ function checkValidServiceWorker(swUrl) {
         response.status === 404 ||
         response.headers.get('content-type').indexOf('javascript') === -1
       ) {
-        // No service worker found. Probably a different app. Reload the page.
+        // No service worker found. Reload the page.
         navigator.serviceWorker.ready.then(registration => {
           registration.unregister().then(() => {
             window.location.reload();
